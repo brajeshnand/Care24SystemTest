dc03942001398373dea269871fd9018d4d00a485
Sri Krishna <srikrishna.nunna@gmail.com>
2016-02-13 11:21:50 +0530

    First Commit

---
+++ app/src/test/java/com/brajeshnand2k8/care24systemtest/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.brajeshnand2k8.care24systemtest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
